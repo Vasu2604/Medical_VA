d25c135b975287a7168084e49244d50f9f752fe5
Vasav Patel <62866375+Vasu2604@users.noreply.github.com>
2025-04-22 11:07:36 -0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'NLP Project',
+    version= '0.0.0',
+    author= 'Vasu Patel',
+    author_email= 'patelvasav2604@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
